fd9ee1640886c9e5fee5603bf6447db2c04e729d
fernandoAraujo <6fefe6@gmail.com>
2022-07-25 11:35:01 -0300

    exercícios 4.2

---
+++ modulo1/bloco4/4.2/07.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor=200;
+for(let index=0; index<numbers.length; index++){
+    if(menor >= numbers[index]){
+        menor = numbers[index];
+    }
+}
+console.log(menor);
---
+++ modulo1/bloco4/4.2/08.js
@@ -0,0 +1,7 @@
+let numbers = [];
+let valores=1;
+for(let index=0;numbers[25];index++){
+    numbers.push(valores);
+    valores++;
+}
+console.log(numbers);
